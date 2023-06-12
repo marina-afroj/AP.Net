9fc744df7d11cd02c7f60b31a294aaa4874099d7
Marina Afroj <136266800+marina-afroj@users.noreply.github.com>
2023-06-13 05:12:25 +0600

    HomeTask

---
+++ HomeTask/HomeTask/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using HomeTask.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace HomeTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.CompanyName = "Nascenia";
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            ServicesModel s1 = new ServicesModel()
+            {
+                Title = "Web Development",
+                Description = "Ruby on Rails,php,.Net,Nodejs"
+
+            };
+
+            ServicesModel s2 = new ServicesModel()
+            {
+
+                Title = "UI/UX Design",
+                Description = "Requirment Generation,Wireframing,Design for web and mobile"
+
+            };
+            ServicesModel[] services = new ServicesModel[] { s1, s2 };
+
+            return View(services);
+        }
+
+        public ActionResult Terms_Members()
+        {
+            MembersModel[] members = new MembersModel[]
+            {
+                new MembersModel("Md. Shahadat Hossain","html" ),
+                new MembersModel("Aditi Debnath","php" ),
+                new MembersModel("Farhana Tasneem",".Net" )
+            };
+            return View(members);
+        }
+        public ActionResult Contact() => View();
+    }
+}
---
+++ HomeTask/HomeTask/Models/MembersModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeTask.Models
+{
+    public class MembersModel
+    {
+        public string Name { get; set; }
+        public string Developer_Lang { get; set; }
+
+        public MembersModel(string name, string devLang)
+        {
+            Name = name;
+            Developer_Lang = devLang;
+        }
+    }
+
+}
